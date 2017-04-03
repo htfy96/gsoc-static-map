a79a84dd13354c027a3a4c23612ed797c33edb3e
Vic Luo <vicluo96@gmail.com>
2017-04-03 14:31:07 +0800

    use initializer_list in static_map constructor

--- include/static_map/static_map.hpp
+++ include/static_map/static_map.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <climits>
+#include <initializer_list>
 #include <iostream>
 #include <stdexcept>
 #include <type_traits>
@@ -37,7 +38,7 @@ namespace static_map
         static constexpr std::size_t Size = SIZE;
         static constexpr std::size_t TableSize = 1.618 * Size;  // :) by instinct!
 
-        using PairT = std::pair<KeyType, MappedType>;
+        using ValueType = std::pair<KeyType, MappedType>;
 
       private:
         struct Item
@@ -67,7 +68,7 @@ namespace static_map
         MappedType map_buf[TableSize];
         bool has_buf[TableSize];
 
-        constexpr void try_insert(const PairT &p, size_t pos)
+        constexpr void try_insert(const ValueType &p, size_t pos)
         {
             if(has_buf[pos])
                 try_insert(p, (pos + 1) % TableSize);
@@ -82,16 +83,20 @@ namespace static_map
         }
 
         template <typename... Us>
-        constexpr void init(const PairT &p, Us &&... args)
+        constexpr void init(const ValueType &p)
         {
             std::size_t pos = static_hash(p.first) % TableSize;
             try_insert(p, pos);
-            init(std::forward<Us>(args)...);
+            // init(std::forward<Us>(args)...);
         }
 
         constexpr void init() {}
 
-        static constexpr std::size_t at_oor() { throw std::out_of_range("OOR"); return 0; }
+        static constexpr std::size_t at_oor()
+        {
+            throw std::out_of_range("OOR");
+            return 0;
+        }
 
         constexpr std::size_t at_linear(size_t pos, KeyT key, size_t start_pos) const
         {
@@ -112,13 +117,14 @@ namespace static_map
         }
 
       public:
-        template <typename... Us>
-        constexpr StaticMap(Us &&... args)
+        constexpr StaticMap(std::initializer_list<ValueType> il)
             : key_buf{}
             , map_buf{}
             , has_buf{}
         {
-            init(std::forward<Us>(args)...);
+
+            for(const auto &v : il)
+                init(v);
         }
 
         constexpr const MappedType &operator[](KeyT key) const { return map_buf[at_impl(key)]; }
